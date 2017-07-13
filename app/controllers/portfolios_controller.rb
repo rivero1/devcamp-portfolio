bc865c930384895f077b1447c27eea759d235e4e
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 16:18:45 +0000

    Implemented partial for the nav component

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,6 @@
 class PortfoliosController < ApplicationController
   layout 'portfolio'
+  
   def index
     @portfolio_items = Portfolio.all
   end
@@ -27,6 +28,7 @@ class PortfoliosController < ApplicationController
 
   def edit
     @portfolio_item = Portfolio.find(params[:id])
+
   end
 
   def update
